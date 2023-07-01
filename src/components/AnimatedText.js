1835b31f7d810001bfcf1e7bd2ec91506be07c6b
Violeta <holasoyviole@gmail.com>
2023-07-01 14:59:52 -0300

    texto animado pero no se va a mostrar todavia

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const quote = {
   initial: {
-    opacity: 0,
+    opacity: 1,
   },
   animate: {
     opacity: 1,
@@ -33,10 +33,9 @@ const AnimatedText = ({ text, className = "" }) => {
     <div
       className="w-full mx-auto py-2 flex items-center justify-center text-center
     overflow-hidden
-    "
-    >
+    ">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold capitalize text-8x1  ${className}`}
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl  ${className}`}
         variants={quote}
         initial="initial"
         animate="animate"
