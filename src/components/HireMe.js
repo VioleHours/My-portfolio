89eec1c8ebcdbf5db92786b9b89065afdaa18d2b
Violeta <holasoyviole@gmail.com>
2023-07-01 15:04:06 -0300

    extras necesarios, agregados

---
+++ src/components/HireMe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { CircularText } from "./Icons";
+import Link from 'next/link'
+
+const HireMe = () => {
+  return (
+    <div
+      className="fixed left-4 bottom-4
+    flex items-center justify-center overflow-hidden
+    "
+    >
+      <div className="w-48 h-auto flex items-center justify-center relative">
+        {/* <CircularText className={"fill-dark animate-spin-slow"} /> */}
+        <Link href='violehours@gmail.com' className="flex items-center justify-center
+        absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+        ">
+          Hire Me
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HireMe;
