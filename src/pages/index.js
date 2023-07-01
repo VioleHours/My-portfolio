8474d11ea11cf530f48ced1b8b44373e8304485d
Violeta <holasoyviole@gmail.com>
2023-07-01 15:01:42 -0300

    index acomodado

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
 
 export default function Home() {
   return (
@@ -24,17 +27,33 @@ export default function Home() {
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                className="!text-6x1 !text-left"
+                className="!text-6xl !text-left"
               />
-              <p>
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link href="/dummy.pdf" target={"_blank"}
+                className="flex items-center bg-dark text-light p-2.5 px-6 
+                rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+                border-solid border-transparent hover:border-dark 
+                "
+                download={true}
+                >Resume
+                <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link href="mailto:violehours@gmail.com" target={"_blank"}
+                className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >Contact</Link>
+              </div>
             </div>
           </div>
         </Layout>
+        
+        <HireMe/>
       </main>
     </>
   );
