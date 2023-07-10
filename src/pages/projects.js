53cdeee5ce11d99d71a4256a68c404b0bda4a735
Violeta <holasoyviole@gmail.com>
2023-07-10 14:34:09 -0300

    pagina de proyectos agregada

---
+++ src/pages/projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from 'next/head'
+
+const projects = () => {
+  return (
+    <>
+      <Head>
+        <title> VioleHours | Projects Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  );
+};
+
+export default projects;
