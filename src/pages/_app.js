a0151580de7a836b6f9873e170b2545a0287edec
Violeta <holasoyviole@gmail.com>
2023-07-09 22:07:51 -0300

    probando app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer"; 
 import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
+  subsets: ["latin"], 
   variable: "--font-mont",
 });
 
