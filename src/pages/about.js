92eb290373a9bde8d1a01eadd82122625daab12d
Violeta <holasoyviole@gmail.com>
2023-07-10 14:30:48 -0300

    about completo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,34 +1,36 @@
 import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
-import Image from 'next/image'
+import Image from "next/image";
 import React, { useEffect, useRef } from "react";
-import profilePic from '../../public/images/profile/developer-pic-2.jpg'
-import { useInView, useMotionValue, useSpring  } from "framer-motion";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
 
-const AnimatedNumbers = ({value}) => {
-  const ref= useRef(null);
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
 
   const motionValue = useMotionValue(0);
-  const springValue = useSpring(motionValue, {duration: 3000})
-  const isInView = useInView(ref, {once: true});
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
 
-  useEffect (() => {
-    if(isInView) {
-      motionValue.set(value)
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
     }
-  }, [isInView, value, motionValue])
+  }, [isInView, value, motionValue]);
 
   useEffect(() => {
     springValue.on("change", (latest) => {
-      if(ref.current && latest.toFixed(0) <= value){
+      if (ref.current && latest.toFixed(0) <= value) {
         ref.current.textContent = latest.toFixed(0);
       }
-    })
-  }, [springValue, value])
-  return <span ref={ref}></span>
-}
+    });
+  }, [springValue, value]);
+  return <span ref={ref}></span>;
+};
 
 const about = () => {
   return (
@@ -39,45 +41,65 @@ const about = () => {
       </Head>
       <main className="flex w-full flex-col items-center justify-center">
         <Layout className="pt-16">
-          <AnimatedText text="Passion Fuels Purpose!" className="mb-16"/>
+          <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
           <div className="grid w-full grid-cols-8 gap-16">
             <div className="col-span-3 flex flex-col items-start justify-start">
-                <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">Biography</h2>
-                <p className='font-medium'>
-                  Hi, I&apos;m Violeta Sol Arias Hours, a web developer with a special interest in creating functional and  
-                  appealing digital experiences. With a few months of experience in the indstry. I&apos;m always on the lookout for new oportunities to expand
-                  my knowledge and skills. My goal is to woork alongside my clients to turn their ideas into reality and deliver high-quality web solutions.
-                </p>
-                <p className='my-4 font-medium'>
-                  I believe that development and design are about more than just making things look pretty - they&apos;re about solving problems 
-                  and creating intuitive, enjoyable experiences for users.
-                </p>
-                <p className='font-medium'>
-                  Whether I&apos;m working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to 
-                  every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
-                </p>
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi, I&apos;m Violeta Sol Arias Hours, a web developer with a
+                special interest in creating functional and appealing digital
+                experiences. With a few months of experience in the indstry.
+                I&apos;m always on the lookout for new oportunities to expand my
+                knowledge and skills. My goal is to woork alongside my clients
+                to turn their ideas into reality and deliver high-quality web
+                solutions.
+              </p>
+              <p className="my-4 font-medium">
+                I believe that development and design are about more than just
+                making things look pretty - they&apos;re about solving problems
+                and creating intuitive, enjoyable experiences for users.
+              </p>
+              <p className="font-medium">
+                Whether I&apos;m working on a website, mobile app, or other
+                digital product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
             </div>
 
             <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-2xl bg-dark"/> 
-              <Image src={profilePic} alt="VioleHours" className='w-full h-auto rounded-2xl' />
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-2xl bg-dark" />
+              <Image
+                src={profilePic}
+                alt="VioleHours"
+                className="w-full h-auto rounded-2xl"
+              />
             </div>
-             <div className="col-span-2 flex flex-col items-end justify-between">
-            <div className="flex flex-col items-end justify-center">
-              <span className="inline-block text-7xl font-bold">
-                <AnimatedNumbers value={1} />+
-              </span>
-              <h2 className="text-xl font-medium capitalize text-dark/75">years of experience</h2>
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={1} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  years of experience
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={3} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  projects completed
+                </h2>
+              </div>
             </div>
-            <div className="flex flex-col items-end justify-center">
-              <span className="inline-block text-7xl font-bold">
-              <AnimatedNumbers value={3} />+
-              </span>
-              <h2 className="text-xl font-medium capitalize text-dark/75">projects completed</h2>
-            </div> 
-             </div>
           </div>
           <Skills />
+          <Experience />
+          <Education />
         </Layout>
       </main>
     </>
