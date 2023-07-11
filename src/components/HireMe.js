ab03c60a098e3d4ef9bdd2ef51977de31e75e665
Violeta <holasoyviole@gmail.com>
2023-07-11 19:30:25 -0300

    Certificates responsive

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -7,6 +7,7 @@ const HireMe = () => {
     <div
       className="fixed left-4 bottom-4
     flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
+    sm:right-0
     "
     >
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -14,13 +14,13 @@ const FramerImage = motion(Image)
 const FeaturedCertificates = ({ img, title, time, link }) => {
   return (
     <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light'>
-        <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
-      rounded-br-3xl dark:bg-light
+        <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
+      
       " />
       <Link
         href={link}
         target="_blank"
-        className="w-full inline-block cursor-pointer overflow-hidden rounded-lg"
+        className="w-full inline-block cursor-pointer overflow-hidden rounded-lg lg:w-full"
       >
         <FramerImage src={img} alt={title} className="w-full h-auto" 
         whileHover={{scale:1.05}}
@@ -29,8 +29,8 @@ const FeaturedCertificates = ({ img, title, time, link }) => {
       </Link>
 
       <Link href={link} target="_blank">
-        <h2 className='capitalize text-2xl font-bold my-2 hover:underline'>{title}</h2>
-        <span className='text-primary dark:text-primaryDark font-semibold'>{time}</span>
+        <h2 className='capitalize text-2xl font-bold my-2 hover:underline sm:text-lg'>{title}</h2>
+        <span className='text-primary dark:text-primaryDark font-semibold sm:text-sm'>{time}</span>
       </Link>
     </li>
   );
@@ -48,12 +48,12 @@ const certificates = () => {
       overflow-hidden dark:text-light
       "
       >
-        <Layout className="pt-16">
+        <Layout className="pt-16 ">
           <AnimatedText
             text="Changing the world starts with you!"
-            className="mb-16"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
-          <ul className="grid grid-cols-2 gap-16">
+          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <FeaturedCertificates 
             img={certificado1}
             title='Teaching Assistant Certificate'
