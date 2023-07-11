46f016be7837cd5aa1a66b565ac006b0392e11cb
Violeta <holasoyviole@gmail.com>
2023-07-11 17:18:04 -0300

    responsive primera parte

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -16,14 +16,14 @@ const quote = {
 
 const singelWord = {
   initial: {
-    opacity:0,
-    y:50,
+    opacity: 0,
+    y: 50,
   },
   animate: {
-    opacity:1,
-    y:0,
+    opacity: 1,
+    y: 0,
     transition: {
-      duration:1,
+      duration: 1,
     },
   },
 };
@@ -32,15 +32,17 @@ const AnimatedText = ({ text, className = "" }) => {
   return (
     <div
       className="w-full mx-auto py-2 flex items-center justify-center text-center
-    overflow-hidden
-    ">
+    overflow-hidden sm:py-0
+    "
+    >
       <motion.h1
         className={`inline-block w-full text-dark font-bold capitalize text-8xl dark:text-light ${className}`}
         variants={quote}
         initial="initial"
         animate="animate"
       >
-        {text.split(" ").map((word, index) => (
+        {
+        text.split(" ").map((word, index) => (
           <motion.span
             key={word + "-" + index}
             className="inline-block"
@@ -50,7 +52,8 @@ const AnimatedText = ({ text, className = "" }) => {
           >
             {word}&nbsp;
           </motion.span>
-        ))}
+        ))
+        }
       </motion.h1>
     </div>
   );
